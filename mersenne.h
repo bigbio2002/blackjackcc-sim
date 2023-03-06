9d06be4c82a2b095ebbf66bbbe86e4feae79f029
Harry Weinstein <449154+bigbio2002@users.noreply.github.com>
2023-03-06 04:22:25 -0500

    Update mersenne.h

--- mersenne.h
+++ mersenne.h
@@ -9,49 +9,83 @@ public:
 	}
 	void srand(unsigned int seed)
 	{
+index = 624;
+
 		MT[0] = seed;
-		static int i;
-		for(i=1;i<624;i++)
+		for(unsigned int i=1;i<624;i++)
 		{
-			MT[i] = 1812433253u * ( MT[i-1] ^ (MT[i-1]>>30) ) + i;
+			MT[i] = (1812433253u * (MT[i-1] >> 30)) + i; //TODO: verify order of operations
 		}
 	}
 	unsigned int rand()
 	{
-		if(index == 0)
+		if(index >= 624)
+		{
+			if(index > 624)
+				exit(1);
+
 			generateNumbers();
+		}
 
-		static unsigned int y;
+		unsigned int y;
 		y = MT[index];
-		y = y^(y>>11);
-		y = y^((y<<7) & 2636928640u);
-		y = y^((y<<15) & 4022730752u);
-		y = y^(y>>18);
+		y = y^(y >> 11);
+		y = y^((y << 7) & 2636928640u);
+		y = y^((y << 15) & 4022730752u);
+		y = y^(y >> 18);
 
-		index = (index+1)%624;
+		index = ++index;
 		return y;
 	}
 	void generateNumbers()
 	{
-		static int i;
-		for(i=0;i<624;i++)
+		for(unsigned int i=0;i<624;i++)
 		{
 			//-2147483648 = 32nd bit of a 32 bit integer
 			//2147483647 = bits 1-31 of an int
-			static unsigned int y;
-			y = (MT[i] & 0x80000000) + ((MT[i+1]%624) & 0x7FFFFFFF);
-			MT[i] = MT[(i+397)%624] ^ (y>>1);
+			unsigned int x, xA;
 
-			//if(y%2 == 1)	//if number is odd
-			if(y & 0x00000001)
+			x = (MT[i] & upper_mask) | (MT[(i+1) % 624] & lower_mask);
+			xA = (x >> 1);
+
+			if(x & 1)	//if number is odd
 			{
-				MT[i] = MT[i]^2567483615u;
+				xA = (xA ^ 2567483615u);
 			}
+
+			MT[i] = (MT[(i+397) % 624] ^ xA);
 		}
+
+	index = 0;
 	}
+
 private:
-	int index;
+/*
+MT19937 coefficients:
+
+(w, n, m, r) = (32, 624, 397, 31)
+a = 0x9908b0df
+(u, d) = (11, 0xffffffff)
+(s, b) = (7, 0x9d2c5680)
+(t, c) = (15, 0xefc60000)
+l = 18
+
+f = 1,812,433,253
+*/
+
+/*
+MT19937-64 coefficients:
+
+TODO: fill in
+*/
+
+	unsigned int index;
 	unsigned int MT[624];
+
+	static const unsigned int lower_mask = 0x7FFFFFFF; // 2147483647u
+	static const unsigned int upper_mask = 0x80000000; // 2147483648u
+
+	void generateNumbers();
 };
 
 #endif
